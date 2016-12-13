0d58d5ae85901c8544c3252167155f74282e2f89
LyudmilaRussiyan <lalka_best@ukr.net>
2016-12-13 12:13:40 +0200

    Create Mainland.java

---
+++ Mainland.java
@@ -0,0 +1,12 @@
+public class Mainland
+{
+    private String name;
+
+    public Mainland(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
