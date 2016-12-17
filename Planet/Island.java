96aeee20e36e413b3ee11cbf13307117fbecb058
LyudmilaRussiyan <lalka_best@ukr.net>
2016-12-17 09:11:29 +0200

    Update Island.java

--- Planet/Island.java
+++ Planet/Island.java
@@ -1,1 +1,12 @@
+public class Island {
 
+    private String name;
+
+    public Island(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
